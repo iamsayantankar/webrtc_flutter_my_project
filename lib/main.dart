431a3544cc3ffd02a38fdd713f1e0cb690724b53
sayan <sayantankar01@gmail.com>
2023-04-14 02:23:22 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'dart:core';
+
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:webrtc_flutter_my_project/webrtc_02/main.dart';
+import 'package:webrtc_flutter_my_project/webrtc_03/main.dart';
+
+
+//
+// void main() => runApp(
+//     new MyApp()
+// );
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
